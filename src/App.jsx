0ea9a43ffbd7035d50b1deef479631b85a5753f6
Shaheem Niyaz <shaheembn@gmail.com>
2025-07-30 12:28:26 +0530

    feat: modularize global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,12 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import { createGlobalStyle, css } from "styled-components";
 
 import { AppPosition } from "@/utils/constants";
 import { useFetch } from "@/hooks";
 
 import { Header, Navigator } from "@/components";
 
-const GlobalStyle = createGlobalStyle(css`
-  :root {
-    /* Colors */
-    --color-background: hsl(358deg, 74%, 94%);
-    --color-text: hsl(240deg, 21%, 15%);
-    --color-accent: hsl(355deg, 80%, 70%);
-
-    --color-light: hsl(220deg, 23%, 95%);
-    --color-dark: hsl(240deg, 23%, 9%);
-
-    --color-accent-dark: hsl(355deg, 70%, 60%);
-    --color-light-dim: hsla(240deg, 100%, 100%, 40%);
-    --color-light-dimmer: hsla(240deg, 100%, 100%, 20%);
-
-    --color-green: hsl(96deg, 44%, 60%);
-    --color-red: hsl(359deg, 68%, 60%);
-
-    --color-green-dark: hsl(96deg, 44%, 50%);
-    --color-red-dark: hsl(359deg, 68%, 50%);
-
-    /* Fonts */
-    --font-highlight: "Fjalla One";
-    --font-normal: "Space Grotesk";
-  }
-
-  body {
-    background-color: var(--color-background);
-    color: var(--color-text);
-  }
-
-  #root {
-    min-height: calc(100svh - 64px);
-    margin: 0 auto;
-    margin-bottom: 64px;
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    font-family: var(--font-normal), system-ui, sans-serif;
-  }
-
-  button {
-    cursor: pointer;
-  }
-
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-`);
+import { RootStyle } from "@/styles";
 
 const App = () => {
   const productsData = useFetch("https://fakestoreapi.com/products");
@@ -67,7 +16,7 @@ const App = () => {
 
   return (
     <>
-      <GlobalStyle />
+      <RootStyle />
       <Header />
       <Outlet
         context={{
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,7 @@
+import { RootStyle } from "./RootStyle";
+
 import { Page } from "./Page";
 import { Heading } from "./Heading";
 import { Info } from "./Info";
 
-export { Page, Heading, Info };
+export { RootStyle, Page, Heading, Info };
